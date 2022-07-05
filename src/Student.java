096d5878820343c4b3f92c79e6710f87dc95e25a
Anton Klimenko <antonyklimenko@gmail.com>
2022-07-05 17:38:05 +0300

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,36 @@
+public class Student {
+    private String name;
+    public int age;
+    public double mark = 4;
+    public boolean alive;
+
+    // default constructor (with no args)
+    public Student(){
+        name = "no name";
+        age = 16;
+        mark = 4;
+        alive = true;
+    }
+
+    // full constructor with params/args
+    public Student(String name, int age, double mark, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.mark = mark;
+        this.alive = alive;
+    }
+
+    // copy-constructor
+    public Student(Student student){
+        name = student.name;
+        age = student.age;
+        mark = student.mark;
+        alive = student.alive;
+    }
+
+    public String getInfo(){
+        return name + ": age = " + age
+                + ", mark = " + mark
+                + ", is alive = " + (alive ? "yes" : "no");
+    }
+}
